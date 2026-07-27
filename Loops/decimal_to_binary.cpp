a544f65708328e3ef10df168edcfa577e8487e04
Pratiksha Kathayat <Kathayatpratiksha@gmail.com>
2026-07-27 22:55:12 +0530

    Add files via upload

---
+++ Loops/decimal_to_binary.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num;
+int sum=0;
+cin>>num;
+int mul = 1;
+while (num>0)
+{
+    int digit = num%2;
+    sum = sum + digit * mul;
+    num = num/2;
+    mul = mul*10;
+
+}
+{ 
+cout<<sum;}
+return 0;
+} 
+
