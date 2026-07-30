d8f384e169297d8f111b5aefb755e8205892d378
pratiksha <kathayatpratiksha@gmail.com>
2026-07-30 22:48:30 +0530

    Added Arrays folder

---
+++ Arrays/printarrayduplicacy.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={2,3,8,7,4};
+{
+    for(int i=0; i<5; i++)
+{
+    for(int j=0; j<=5; j++)
+      cout<<arr[j]<<" ";
+       cout<<endl;
+    }
+    return 0;
+}
+}
